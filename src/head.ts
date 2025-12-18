bc6c9c939d966f065b27e6e2759f93d59dce3162
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-18 01:11:35 -0800

    Upgrade modules

--- src/head.ts
+++ src/head.ts
@@ -23,7 +23,7 @@ function run(endpoint: string, callback: HeadCallback) {
   });
 }
 
-const worker = noHTTPS ? bind('>0', workerPath) : run;
+const worker = noHTTPS ? bind('>0', workerPath, { callbacks: true }) : run;
 
 export default function head(endpoint: string): Promise<HeadResponse>;
 export default function head(endpoint: string, callback: HeadCallback): void;
